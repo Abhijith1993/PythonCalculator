3f6456756089907dee5d9ed32dad660768daeaf3
Abhijith1993 <64603617+Abhijith1993@users.noreply.github.com>
2020-06-30 22:12:44 +0300

    Now C funtion and font update

--- Calcu.py
+++ Calcu.py
@@ -11,6 +11,13 @@ def input_number(number, equation):
    expression = expression + str(number)
    equation.set(expression)
 
+def clear1(number, equation):
+    global expression
+    length = len(expression)
+    expression = expression[0 : -1]
+    equation.set(expression)
+
+
 class Calcu:
 
     def gui_structure(self):
@@ -30,58 +37,75 @@ class Calcu:
         txt2 = Entry(screen,textvariable=equation, width=17, relief=FLAT, justify='right', bd=26, font=('Arial', 26, 'bold'), bg='orange')
         txt2.grid(column=0, row=1, padx=5, pady=0)
         # -------------buttons--------------------------
-        button7 = Button(screen, text='7', width=8, height=3,command=lambda: input_number(7, equation))
+        button7 = Button(screen, text='7', width=3, height=1,
+                         command=lambda: input_number(7, equation), font="Verdana 19 bold")
         button7.place(x=15, y=290)
 
-        button8 = Button(screen, text='8', width=8, height=3,command=lambda: input_number(8, equation) )
+        button8 = Button(screen, text='8', width=3, height=1,
+                         command=lambda: input_number(8, equation),font="Verdana 19 bold" )
         button8.place(x=115, y=290)
 
-        button9 = Button(screen, text='9', width=8, height=3,command=lambda: input_number(9, equation) )
+        button9 = Button(screen, text='9', width=3, height=1,
+                         command=lambda: input_number(9, equation), font="Verdana 19 bold" )
         button9.place(x=215, y=290)
 
-        button4 = Button(screen, text='4', width=8, height=3,command=lambda: input_number(4, equation) )
+        button4 = Button(screen, text='4', width=3, height=1,
+                         command=lambda: input_number(4, equation),font="Verdana 19 bold" )
         button4.place(x=15, y=370)
 
-        button5 = Button(screen, text='5', width=8, height=3,command=lambda: input_number(5, equation) )
+        button5 = Button(screen, text='5', width=3, height=1,
+                         command=lambda: input_number(5, equation),font="Verdana 19 bold" )
         button5.place(x=115, y=370)
 
-        button6 = Button(screen, text='6', width=8, height=3,command=lambda: input_number(6, equation) )
+        button6 = Button(screen, text='6', width=3, height=1,
+                         command=lambda: input_number(6, equation),font="Verdana 19 bold" )
         button6.place(x=215, y=370)
 
-        button1 = Button(screen, text='1', width=8, height=3,command=lambda: input_number(1, equation) )
+        button1 = Button(screen, text='1', width=3, height=1,
+                         command=lambda: input_number(1, equation),font="Verdana 19 bold" )
         button1.place(x=15, y=450)
 
-        button2 = Button(screen, text='2', width=8, height=3,command=lambda: input_number(2, equation) )
+        button2 = Button(screen, text='2', width=3, height=1,
+                         command=lambda: input_number(2, equation),font="Verdana 19 bold" )
         button2.place(x=115, y=450)
 
-        button3 = Button(screen, text='3', width=8, height=3,command=lambda: input_number(3, equation) )
+        button3 = Button(screen, text='3', width=3, height=1,
+                         command=lambda: input_number(3, equation),font="Verdana 19 bold" )
         button3.place(x=215, y=450)
 
-        button0 = Button(screen, text='0', width=22, height=3,command=lambda: input_number(0, equation) )
+        button0 = Button(screen, text='0', width=9, height=1,
+                         command=lambda: input_number(0, equation),font="Verdana 19 bold" )
         button0.place(x=15, y=530)
 
-        buttondot = Button(screen, text='.', width=8, height=3,command=lambda: input_number(".", equation) )
+        buttondot = Button(screen, text='.', width=3, height=1,
+                           command=lambda: input_number(".", equation),font="Verdana 19 bold" )
         buttondot.place(x=215, y=530)
 
-        buttoneq = Button(screen, text='=', width=8, height=3, )
+        buttoneq = Button(screen, text='=', width=3, height=1 ,font="Verdana 19 bold" )
         buttoneq.place(x=315, y=450)
 
-        buttonpl = Button(screen, text='+', width=8, height=8,command=lambda: input_number("+", equation) )
+        buttonpl = Button(screen, text='+', width=3, height=3,
+                          command=lambda: input_number("+", equation),font="Verdana 19 bold" )
         buttonpl.place(x=315, y=290)
 
-        buttonml = Button(screen, text='X', width=8, height=3,command=lambda: input_number("X", equation) )
+        buttonml = Button(screen, text='X', width=3, height=1,
+                          command=lambda: input_number("x", equation),font="Verdana 19 bold" )
         buttonml.place(x=315, y=210)
 
-        buttondi = Button(screen, text='÷', width=8, height=3,command=lambda: input_number("/", equation) )
+        buttondi = Button(screen, text='÷', width=3, height=1,
+                          command=lambda: input_number("/", equation),font="Verdana 19 bold" )
         buttondi.place(x=215, y=210)
 
-        buttonro = Button(screen, text='√', width=8, height=3,command=lambda: input_number("√", equation) )
+        buttonro = Button(screen, text='√', width=3, height=1,
+                          command=lambda: input_number("√", equation),font="Verdana 19 bold" )
         buttonro.place(x=115, y=210)
 
-        buttonmod = Button(screen, text='%', width=8, height=3,command=lambda: input_number("%", equation) )
+        buttonmod = Button(screen, text='%', width=3, height=1,
+                           command=lambda: input_number("%", equation),font="Verdana 19 bold" )
         buttonmod.place(x=15, y=210)
 
-        buttondel = Button(screen, text='DEL', width=8, height=3,)
+        buttondel = Button(screen, text='C', width=3, height=1,
+                           command=lambda:clear1(1,equation),font="Verdana 19 bold")
         buttondel.place(x=315, y=530)
 
         screen.mainloop()
